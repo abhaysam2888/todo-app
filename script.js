ea3fb980b963d16a25a9b02550edf6477f7625a7
abhaysam2888 <abhaysam2888@gmail.com>
2024-03-18 19:03:37 +0530

    all file added

--- script.js
+++ script.js
@@ -1,59 +1,66 @@
-// Retrieve the input field and buttons
-const todoInput = document.querySelector('#todoInput');
-const addBtn = document.querySelector('#addBtn');
-const date = document.querySelector('#date');
-const todoList = document.querySelector('#todoList');
+let inputText = document.querySelector('#inputText')
+let inputDate = document.querySelector('#inputDate')
+let addBtn = document.querySelector('#addBtn')
+let todoList = document.querySelector('#todoList')
 
+let todoArray = []
 
-// Initialize empty array and display existing todos from localStorage
- let arr = JSON.parse(localStorage.getItem('todos')) || [];
- displayItem()
+window.addEventListener('load', () => {
+  
+    let StoreTodo = JSON.parse(localStorage.getItem('todos'))
 
-//  we can use the below code if not underStand the above code
+    StoreTodo ? todoArray = StoreTodo : console.log('emty localStorage');
 
-// window.addEventListener('load', () => {
-//   let todos = JSON.parse(localStorage.getItem('todos'))
-//   todos ? arr = todos : console.log(false);
-//   displayItem()
-// })
-
+    displayItem()
+})
 function addTodo() {
-    let todoText = todoInput.value
-    let todoDate = date.value
-    if (todoText.length !== 0 && todoDate.length !== 0){
-
-      arr.push({item: todoText, dueDate: todoDate});
-
-      localStorage.setItem('todos', JSON.stringify(arr))
-
-      todoInput.value = ""
-      date.value = ""
-      displayItem()
-    }
+  let text = inputText.value
+  let date = inputDate.value
+  if (inputText.value.length !== 0 && inputDate.value.length !== 0) {
+    todoArray.push({item: text, todoDate: date})
+    localStorage.setItem('todos', JSON.stringify(todoArray))
+    inputText.value = ""
+    inputDate.value = ""
+  }
+  displayItem()
 }
 
-
-
- // Function to delete a todo
- function deleteTodo(index) {
-  arr.splice(index, 1);
-  localStorage.setItem('todos', JSON.stringify(arr));
-  displayItem();
-}
 function displayItem() {
-    let newHtml = '';
-    arr.forEach((item, index) => {
-    let items = item.item
-    let dates = item.dueDate
+  let newHtml = "";
+
+  todoArray.forEach((item, index) => {
+    let todoText = item.item
+    let todoDate = item.todoDate
+    let textId = `input_${index}`
+    let dateId = `input_${index}_date`
+
     newHtml += `
-    <div style="display: flex; justify-content: space-between; align-items: center; padding-top: 10px;">
-    <span>${items}</span>
-    <span>${dates}</span>
-    <button onclick="deleteTodo(${index})">Delete</button>
-    </div>
+        <div style="margin-top: 10px;">
+            <input type="text" name="text" id="${textId}" value=${todoText}>
+            <input type="date" name="date" id="${dateId}" value=${todoDate}>
+            <button id="updateBtn" onclick="updateTodo(${index}, '${textId}', '${dateId}')" class="edit">Save</button>
+            <button onclick="deleteTodo(${index})">Delete</button>
+        </div>
     `
   })
   todoList.innerHTML = newHtml;
+}
+
+function deleteTodo(index) {
+  todoArray.splice(index, 1)
+  localStorage.setItem('todos', JSON.stringify(todoArray))
+  displayItem()
+}
+
+function updateTodo(index, textId, dateId) {
+  let textInput = document.querySelector(`#${textId}`)
+  let date = document.querySelector(`#${dateId}`)
+
+  todoArray[index].item = textInput.value
+  todoArray[index].todoDate = date.value
+
+  localStorage.setItem('todos', JSON.stringify(todoArray))
+  displayItem()
 }
 
 addBtn.addEventListener('click', addTodo)
